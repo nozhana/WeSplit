1408cc02adadd98ebf1d2213623f51323a157536
Nozhan Amiri <nozhana@gmail.com>
2022-07-01 23:54:25 +0430

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nozhan Amiri on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

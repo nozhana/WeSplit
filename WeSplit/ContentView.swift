7e5c730c4ab6a897903d974b13b3c569b0d56531
Nozhan Amiri <nozhana@gmail.com>
2022-07-02 12:53:45 +0430

    Added subviews

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelected = Double(tipPercentage)
+        let tipValue = checkAmount / 100.0 * tipSelected
+        let totalCheckAmount = checkAmount + tipValue
+        return totalCheckAmount / peopleCount
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Check amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                } header: {
+                    Text("Check amount")
+                }
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Tip percentage")
+                }
+                Section {
+                    Picker("How many people?", selection: $numberOfPeople) {
+                        ForEach(2..<50) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total per person")
+                }
+            }
+            .navigationTitle(Text("WeSplit"))
+        }
     }
 }
 
